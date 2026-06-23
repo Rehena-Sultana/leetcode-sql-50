3e48ba5f9490a8672665e3d56a45d822e3c25a0f
Rehena-Sultana <rehenashathi29@gmail.com>
2026-06-23 23:28:08 +0600

    updated

---
+++ solutions/595. Big Countries.sql
@@ -0,0 +1,25 @@
+-- ============================================================
+-- Problem #595. Big Countries.sql
+-- Difficulty: Easy
+-- Link: https://leetcode.com/problems/big-countries/description/?envType=study-plan-v2&envId=top-sql-50
+-- ============================================================
+-- World
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | name        | varchar |
+-- | continent   | varchar |
+-- | area        | int     |
+-- | population  | int     |
+-- | gdp         | bigint  |
+-- +-------------+---------+
+
+--A country is big if:
+    -- it has an area of at least three million (i.e., 3000000 km2), or
+    -- it has a population of at least twenty-five million (i.e., 25000000).
+-- Write a solution to find the name, population, and area of the big countries.
+
+-- Write your PostgreSQL query statement below
+
+SELECT name, population, area  FROM World WHERE area>=3000000 OR population>=25000000;
